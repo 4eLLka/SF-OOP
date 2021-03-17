3d3c5b35293c4858659683eac03b490ccfa3c21a
4eLLka <55049689+4eLLka@users.noreply.github.com>
2021-03-18 00:45:14 +0300

    rectangle_new_2d

--- C1 1.10.1-2/rectangle_new.py
+++ C1 1.10.1-2/rectangle_new.py
@@ -1,3 +1,4 @@
+# 1.10.1
 class Rectangle:
     def __init__(self, x, y, width, height):
         self.x = x
@@ -6,8 +7,21 @@ class Rectangle:
         self.height = height
 
     def __str__(self):
-        return f'x: {self.x} \ny: {self.y} \nwidth: {self.width} \nheight {self.height}'
+        return f'x: {self.x} \ny: {self.y} \nwidth: {self.width} \nheight: {self.height}'
 
 
 figure = Rectangle(5, 10, 50, 100)
 print(figure)
+
+# 1.10.2
+class Rectangle_2D:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return f"2D_Rectangle {self.width} {self.height} {self.width * self.height}"
+
+
+figure = Rectangle_2D = Rectangle_2D(5, 10)
+print(Rectangle_2D.get_area())
